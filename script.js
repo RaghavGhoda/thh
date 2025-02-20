da6d317bcd089555e884891c068e825f6f6a2d12
Guru901 <avinashkaur842@gmail.com>
2025-02-20 22:35:39 +0530

    hehe

--- script.js
+++ script.js
@@ -1,19 +1,2 @@
-const navBtn = document.querySelectorAll(".nav-btn");
-
-navBtn.forEach((btn) => {
-  btn.addEventListener("click", handleMenu);
-});
-
-const navDialog = document.getElementById("nav-Dialog");
-
-function handleMenu() {
-  navDialog?.classList.toggle("hidden");
-}
-
-window.addEventListener("resize", () => {
-  if (window.innerWidth > 768) {
-    navDialog?.classList.add("hidden");
-  }
-})
-
-
+console.log("Hello World!");
+console.log("Saini 6kka")
